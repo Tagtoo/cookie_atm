2835012710535c077b305548a879aa4243f0bc33
LittleQ <littleq0903@gmail.com>
2013-12-17 16:13:14 +0800

    settings sample

--- atm_settings.py
+++ atm_settings.py
@@ -1,7 +1,18 @@
-PROXIED_URIS = [
+PROXIED_URLS = [
     "/hello",
-    "/hello2"
+    "/local.html",
+    "/hello3"
 ]
 
-PROXIED_TARGET = "http://localhost:8080"
+SPECIAL_ROUTES = {
+    '/hello4': 'http://localhost:5001',
+    '/hello5': 'http://localhost:5000'
+}
+
+DEFAULT_PROXIED_HOST = "http://localhost:8000"
+
+try:
+    from atm_settings_local import *
+except:
+    pass
 
